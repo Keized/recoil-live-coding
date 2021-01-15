b93d56c51f6d29cfdbdf2c1714b48e5b49270492
Kevin <kevin.fiseneck@live.fr>
2021-01-15 23:13:19 +0100

    add style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import './App.scss';
 import OperationsList from './component/OperationsList';
 import { RecoilRoot } from 'recoil';
 import OperationEditor from './component/OperationEditor';
@@ -7,17 +6,19 @@ import OperationDetails from './component/OperationDetails';
 
 function App() {
     return (
-        <div id="App" className="py-4 min-vh-100" style={{'background': '#F1F1FA'}}>
+        <div id="App" className="min-vh-100">
             <RecoilRoot>
-                <div className="container">
-                    <div className="row mb-4">
-                        <OperationsList />
-                        <OperationDetails />
+                <main>
+                    <div className="container">
+                        <div className="row mb-4">
+                            <OperationsList />
+                            <OperationDetails />
+                        </div>
+                        <div className="row mb-4">
+                            <OperationEditor />
+                        </div>
                     </div>
-                    <div className="row mb-4">
-                        <OperationEditor />
-                    </div>
-                </div>
+                </main>
             </RecoilRoot>
         </div>
     );
